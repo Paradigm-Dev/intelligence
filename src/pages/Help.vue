77f8d2b9dcc0fa918d506a17fd25b7b5f786b22b
Aidan Liddy <42472908+Diddy12310@users.noreply.github.com>
2020-09-29 16:06:25 -0400

    Help page

---
+++ src/pages/Help.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="text-center pt-12">
+    <p>If you require assistance, please...</p>
+    <p>
+      <a
+        class="text-lg"
+        target="_blank"
+        href="https://github.com/Paradigm-Dev/intelligence/issues"
+        >Open an issue in our GitHub repository</a
+      >
+    </p>
+    <p class="italic">OR</p>
+    <a class="text-lg" href="mailto:paradigmdevelop@gmail.com"
+      >Contact support</a
+    >
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Help",
+};
+</script>
+
+<style scoped></style>
